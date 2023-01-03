3c92409b72388cb992e5029077d0a504c19de526
Stepan Sheihas <raksha89320@gmail.com>
2023-01-03 17:52:18 +0200

    add new file

---
+++ first_autotest.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+
+link ="https://rozetka.com.ua/"
+browser = webdriver.Chrome()
+browser.get(link)
+search_string = browser.find_element(By.XPATH, "/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/div/form/div/div/input")
+search_string.send_keys("Генератор")
+time.sleep(5)
+button = browser.find_element(By.XPATH,"/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/div/form/button").click()
+time.sleep(5)
+perevirka = browser.find_element(By.XPATH, "/html/body/app-root/div/div/rz-search/rz-catalog/div/div[1]/rz-search-heading/div/h1").text
+
+print(perevirka)
+
