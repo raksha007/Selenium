666385772c9beefd11eda0aa3b3f7700d350977c
Stepan Sheihas <raksha89320@gmail.com>
2023-01-03 20:01:46 +0200

    Update new file

--- first_autotest.py
+++ first_autotest.py
@@ -9,8 +9,11 @@ search_string = browser.find_element(By.XPATH, "/html/body/app-root/div/div/rz-h
 search_string.send_keys("Генератор")
 time.sleep(5)
 button = browser.find_element(By.XPATH,"/html/body/app-root/div/div/rz-header/rz-main-header/header/div/div/div/form/button").click()
-time.sleep(5)
+time.sleep(10)
 perevirka = browser.find_element(By.XPATH, "/html/body/app-root/div/div/rz-search/rz-catalog/div/div[1]/rz-search-heading/div/h1").text
-
-print(perevirka)
-
+perevirka1 = "«Генератор»"
+if perevirka == perevirka1:
+    print("Тест пройдений")
+else:
+    print("Тест не пройдений")
+    browser.quit()
